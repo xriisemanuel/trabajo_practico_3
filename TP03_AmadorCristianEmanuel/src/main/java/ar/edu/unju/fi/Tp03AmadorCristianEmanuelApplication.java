3be8b8cad7617d49bab2de12c55145b39f8616d0
xriis <xriis@x7i>
2024-05-09 18:06:20 -0300

    commit 1

---
+++ TP03_AmadorCristianEmanuel/src/main/java/ar/edu/unju/fi/Tp03AmadorCristianEmanuelApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp03AmadorCristianEmanuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp03AmadorCristianEmanuelApplication.class, args);
+	}
+
+}
---
+++ TP03_AmadorCristianEmanuel/src/test/java/ar/edu/unju/fi/Tp03AmadorCristianEmanuelApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp03AmadorCristianEmanuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
